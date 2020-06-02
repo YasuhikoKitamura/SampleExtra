19dea1e372e482a6253b161e1a607c5a6fe86b5d
Yasuhiko Kitamura <ykitamura@kwansei.ac.jp>
2020-06-02 16:37:40 +0900

    Initial Commit

---
+++ src/Execute.java
@@ -0,0 +1,15 @@
+class Execute {
+  public static void main(String[] args) {
+    // static クラス
+    Outer.StaticInner staticInner = new Outer.StaticInner();
+    staticInner.setIn(10);
+    System.out.println(staticInner.getIn());
+    
+    // 厳密的なメンバクラス
+    Outer outer = new Outer();
+    Outer.Inner inner = outer.new Inner();
+    inner.setStr("ABC");
+    System.out.println(inner.getStr());
+    		
+  }
+}
---
+++ src/Outer.java
@@ -0,0 +1,28 @@
+class Outer { // 外部クラス
+
+  // メンバクラス➀ static クラス
+  static class StaticInner {
+    private Integer in;
+
+    public Integer getIn() {
+      return in;
+    }
+
+    public void setIn(Integer in) {
+      this.in = in;
+    }
+  }
+
+  // メンバクラス➁ 厳密的なメンバクラス
+  class Inner {
+    private String str;
+
+    public String getStr() {
+      return str;
+    }
+
+    public void setStr(String str) {
+      this.str = str;
+    }
+  }
+}
