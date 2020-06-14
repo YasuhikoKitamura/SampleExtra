d8ca765e45239c1e1fe94ee8cf850e9e4d839ee3
Yasuhiko Kitamura <ykitamura@kwansei.ac.jp>
2020-06-14 18:30:42 +0900

    コメントの追加

--- src/Execute.java
+++ src/Execute.java
@@ -1,15 +1,15 @@
-class Execute {
+class Execute { //Comment
   public static void main(String[] args) {
     // static クラス
     Outer.StaticInner staticInner = new Outer.StaticInner();
     staticInner.setIn(10);
     System.out.println(staticInner.getIn());
-    
+
     // 厳密的なメンバクラス
     Outer outer = new Outer();
     Outer.Inner inner = outer.new Inner();
     inner.setStr("ABC");
     System.out.println(inner.getStr());
-    		
+
   }
 }
